fed076eaef87ac609e7665341f5aa33c1a7dd26a
xuweigui@hotmail.com <Gary0929>
2014-02-11 12:50:16 +1100

    question 93

---
+++ src/com/windrift/scjp/questions001/question93/Yikes.java
@@ -0,0 +1,11 @@
+package com.windrift.scjp.questions001.question93;
+
+/**
+ * @author Gary Xu
+ */
+public class Yikes
+{
+    public static String go(Long n) {return "Long";}
+    public static String go(Short n) {return "Short";}
+    public static String go(int n) {return "int";}
+}
