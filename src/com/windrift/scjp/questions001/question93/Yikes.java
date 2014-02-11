ff3a0c1c7dcf538454d6d543eb9c2c2cebb31d75
xuweigui@hotmail.com <Gary0929>
2014-02-11 17:35:11 +1100

    =>question 99

--- src/com/windrift/scjp/questions001/question93/Yikes.java
+++ src/com/windrift/scjp/questions001/question93/Yikes.java
@@ -8,4 +8,9 @@ public class Yikes
     public static String go(Long n) {return "Long";}
     public static String go(Short n) {return "Short";}
     public static String go(int n) {return "int";}
+    public static void main(String[] args)
+    {
+        short s = 12;
+        System.out.println(go(s));
+    }
 }
