85462f32a5b2537515335a902de8f402973377ff
gary@UbuntuHome <xuweigui@hotmail.com>
2014-06-03 20:50:23 +1000

    more questions

--- src/main/java/com/windrift/java/general/questions001/question147/Base.java
+++ src/main/java/com/windrift/java/general/questions001/question147/Base.java
@@ -6,6 +6,10 @@ package com.windrift.java.general.questions001.question147;
 public class Base
 {
     public static final String FOO = "foo";
+    public String show()
+    {
+        return FOO;
+    }
     public static void main(String[] args)
     {
         Base b = new Base();
@@ -15,8 +19,15 @@ public class Base
         System.out.print(b.FOO);
         System.out.print(s.FOO);
         System.out.print(((Base)s).FOO);
+        System.out.print(((Base)s).show());
     }
     private int a;
 }
 
-class Sub extends Base {public static final String FOO = "bar";}
+class Sub extends Base {
+    public static final String FOO = "bar";
+    public String show()
+    {
+        return FOO;
+    }
+}
