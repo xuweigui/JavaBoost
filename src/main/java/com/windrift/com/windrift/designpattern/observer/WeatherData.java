f236095e416e64312080a4be3ad847453520aa1c
gary@UbuntuHome <xuweigui@hotmail.com>
2014-06-21 21:44:00 +1000

    cleanup

--- src/main/java/com/windrift/com/windrift/designpattern/observer/WeatherData.java
+++ src/main/java/com/windrift/com/windrift/designpattern/observer/WeatherData.java
@@ -3,7 +3,6 @@ package com.windrift.com.windrift.designpattern.observer;
 import java.util.Observable;
 
 public class WeatherData extends Observable{
-	Observable observable;
 	private float temperature;
 	private float humidity;
 	private float pressure;
